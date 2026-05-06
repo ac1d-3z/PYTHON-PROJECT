2041583b5231a59ad9ac3a0acd278427345bab5d
AC1D X <88615896+ac1d-3z@users.noreply.github.com>
2026-05-06 22:03:49 +0600

    Add files via upload

---
+++ JOB1.py
@@ -0,0 +1,50 @@
+#1.1
+print("Hello World")
+
+#1.2
+print("Bangladesh Technical Education Board")
+
+#1.3
+def cst():
+    print("Bangladesh Technical Education Board")
+cst()
+
+#1.4
+a=int(input("Enter the fist number="))
+b=int(input("Enter the second number="))
+sum=a+b
+dif=a-b
+mul=a*b
+div=a/b
+print("a+b",sum)
+print("a-b",dif)
+print("a*b",mul)
+print("a/b",div)
+
+#1.5
+import math
+r=float(input("Enter Radius="))
+area=math.pi*r*r
+print("Trangle Area=",area)
+
+#1.6
+height=float(input("Enter Height ="))
+wight=float(input("Enter Wight ="))
+area=0.5*height*wight
+print("Trangle Area =",area)
+
+#1.7
+lenght=float(input("Enter Lenght ="))
+wight=float(input("Enter Wight ="))
+area=lenght*wight
+print("Ractangle Area =",area)
+
+#1.8
+celsius=float(input("Enter the Celsius Temperature ="))
+fahrenheit=(celsius*1.8)+32
+print("Celsius to Fahrenheit Temperature =")
+
+#1.9
+fahrenheit=float(input("Enter the Fahrenheit Temperature ="))
+celsius=(fahrenheit-32)/1.8
+print("Fahrenheit to Celsius Temperature =",celsius)
