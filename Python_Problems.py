c10dd12fd4d48750e640eaf711da8a16874add66
AC1D X <88615896+ac1d-3z@users.noreply.github.com>
2026-07-03 14:21:11 +0000

    1-10 Solved

--- Python_Problems.py
+++ Python_Problems.py
@@ -1,5 +1,5 @@
 #1
-print{"hello orld"}
+print("Hello World")
 
 #2
 print("S.M. Riadul Karim")
@@ -10,3 +10,40 @@ print("Sadar,Cox\'s Bazar")
 print("Bangladesh")
 
 #4
+print("Name \t \t: S.M. Riadul Karim")
+print("Age \t \t: 18")
+print("District \t: Cox's Bazar")
+print("Country \t: Bangladesh")
+
+#5
+print("Team Name: \n CBPI Coding Club")
+print("Topic \t: Python Programming")
+print("Location: Cox\'s Bazar, Bangladesh")
+print("Path:\tC:\\Users\\Desktop\\Python_Problems.py")
+
+#6
+print("Fri","july 3","2026",sep=".")
+print("South Hajipara","Sadar","Cox's Bazar","Bangladesh",sep="->")
+
+#7
+print("I\'ll be a",end="")
+print(" Python Programmer",end="")
+print(" in 2026")
+
+#8
+value1 = int(input("Enter first Value: "))
+value2 = int(input("Enter second Value: "))
+sum = value1 + value2
+print("The sum is:", sum)
+
+#9
+value3 = int(input("Enter first Value: "))
+value4 = int(input("Enter second Value: "))
+difference = value3 - value4
+print("The difference is:", difference)
+
+#10
+value5 = int(input("Enter first Value: "))
+value6 = int(input("Enter second Value: "))
+product = value5 * value6
+print("The product is:", product)
